fc5f476b147212d1ecfbd907749c8c61163c740e
muxsina123 <boboxonova2009@gmail.com>
2023-01-01 00:38:37 +0500

    3 elementli massiv

--- index1.php
+++ index1.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <form action="index2.php"  method="POST"> 
-       zaryad%<input type="text" name="zaryad"   id="">
-       <br>
-         zaryatga ulanganmi?<input type="checkbox" name="zaryad" value="a"id="">
-         <br>
-         <button type="submit">yuborish</button>
-     </form>
-</body>
-</html>
+<?php
+$massiv = array(10, 20, 30); 
+for ($i = 0; $i < count($massiv); $i++) {
+    echo $massiv[$i] . "<br>";
+}
+?>
