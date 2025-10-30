9187283396842b6c8cfef1bbfff449f13e80841c
muxsina123 <boboxonova2009@gmail.com>
2025-10-30 17:35:19 +0500

    yangilanish

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
-$cars = array("Lambargine", "BMW", "Mers");
+$cars = array("Lambargine", "BMW", "Mers", 2009);
 echo $cars[0];
  echo "<br>";
 echo $cars[1];
  echo "<br>";
 echo $cars[2];
+ echo "<br>";
+echo $cars[3];
 
 
 ?>
