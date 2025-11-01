985f9313cea05cb2ebc92260432ac456406e6db2
muxsina123 <boboxonova2009@gmail.com>
2025-11-01 14:14:19 +0500

    Birinchi commit

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
-
-$cars = array("Lambargine", "BMW", "Mers", 2009);
-echo $cars[0];
- echo "<br>";
-echo $cars[1];
- echo "<br>";
-echo $cars[2];
- echo "<br>";
-echo $cars[3];
-
-
+// $cars = array("Lambargine", "BMW", "Mers", 2009);
+// echo $cars[0];
+//  echo "<br>";
+// echo $cars[1];
+//  echo "<br>";
+// echo $cars[2];
+//  echo "<br>";
+// echo $cars[3];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index1.php" method="POST">
+        <input type="number" name="a">
+        <button  type="submit">yuborish</button>
+    </form>
+</body>
+</html>
